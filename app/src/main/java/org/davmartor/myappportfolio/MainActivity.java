30e00490451751470c7ba3cf769fa8b8a3844bfb
David Martin Torres <davmartor@gmail.com>
2016-02-25 13:43:48 +0100

    First commit for Udacity Nanondegree App portfolio

---
+++ app/src/main/java/org/davmartor/myappportfolio/MainActivity.java
@@ -0,0 +1,74 @@
+package org.davmartor.myappportfolio;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view){
+        Context context = getApplicationContext();
+        CharSequence[] text = {
+                "This button will launch Spotify Streamer app!",
+                "This button will launch Super Duo Scores app!",
+                "This button will launch Super Duo Library app!",
+                "This button will launch Build It Bigger app!",
+                "This button will launch XYZ Reader app!",
+                "This button will launch Capstone app!"
+
+        };
+        int duration = Toast.LENGTH_SHORT;
+        int idMessage = Integer.parseInt(view.getTag().toString());
+        Toast toast = Toast.makeText(context, text[idMessage], duration);
+        toast.show();
+    }
+
+
+
+}
